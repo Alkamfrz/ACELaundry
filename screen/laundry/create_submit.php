dfc112dab4a632f69ce0bde207dc85b3c15a1551
CMAngeloZ <silvermercury1@gmail.com>
2023-07-13 09:31:24 +0700

    laundry add

--- screen/laundry/create_submit.php
+++ screen/laundry/create_submit.php
@@ -1,5 +1,5 @@
 <?php
 include_once('../../db_connect_supa.php');
-$result = $pdo->query("insert into orderes (name, pieces, note, date, weight, service, item) values ('$_POST[name]', '$_POST[telp]')");
+$result = $pdo->query("insert into orderes (name, pieces, note, date, weight, s_id, lp_p_id) values ('$_POST[name]', '$_POST[telp]')");
 header("location: create.php");
 ?>
