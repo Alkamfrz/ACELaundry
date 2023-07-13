4640cdffa33bc780d6e82109d76d699bf9fee4bf
CMAngeloZ <silvermercury1@gmail.com>
2023-07-13 09:41:01 +0700

    create edit

--- screen/laundry/create_submit.php
+++ screen/laundry/create_submit.php
@@ -1,5 +1,5 @@
 <?php
 include_once('../../db_connect_supa.php');
-$result = $pdo->query("insert into orderes (name, pieces, note, date, weight, s_id, lp_p_id) values ('$_POST[name]', '$_POST[telp]')");
-header("location: create.php");
+$result = $pdo->query("insert into orderes (name, pieces, note, date, weight, s_id, lp_p_id) values ('$_POST[name]', '$_POST[pieces]', '$_POST[note]', '$_POST[date]', '$_POST[weight]', '$_POST[s_id]', '$_POST[lp_p_id]')");
+header("location: pending.php");
 ?>
